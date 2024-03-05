bc0aa07366be2bda69a46bcc622950c45aacd9e6
HTFDT <almetov_tag@mail.ru>
2024-03-05 21:12:55 +0500

    add group api entities

---
+++ GroupsApi/Domain/Entities/GroupRole.cs
@@ -0,0 +1,30 @@
+﻿using Core.Base.Dal;
+
+namespace Domain.Entities;
+
+
+/// <summary>
+/// сущность внутричатовых ролей, пользователь со специальной ролью может их создавать/редактировать их права/удалять
+/// (ManyToMany с GroupRights)
+/// </summary>
+public class GroupRole : BaseEntityDal<Guid>
+{
+    public required bool IsRevocable { get; set; }
+    // если внутри группы уже есть участник с этой ролью, то выдать её другому участнику нельзя
+    public required bool IsUnique { get; set; }
+    public bool IsDefault { get; set; }
+    public required bool IsAssignableByUsers { get; set; }
+    public bool IsDefaultMemberRole { get; set; }
+    public bool IsDefaultKickedRole { get; set; }
+    public bool IsDefaultOwnerRole { get; set; }
+    public bool IsDefaultLeftRole { get; set; }
+    public required Guid? GroupId { get; set; }
+    public required string Name { get; set; }
+    public string NormalizedName { get; set; } = null!;
+    public required string Description { get; set; }
+
+    public Group? Group { get; set; }
+    public ICollection<Membership> Memberships { get; set; } = null!;
+    public ICollection<Membership> MembershipsWithThisRoleBeforeLeave { get; set; } = null!;
+    public ICollection<GroupRight> GroupRights { get; set; } = null!;
+}
---
+++ GroupsApi/Domain/Entities/Tag.cs
@@ -0,0 +1,16 @@
+﻿using Core.Base.Dal;
+
+namespace Domain.Entities;
+
+
+/// <summary>
+/// сущность тэгов, которые прикрепляются к группам
+/// </summary>
+public class Tag : BaseEntityDal<Guid>
+{
+    public required string Name { get; set; }
+    public string? NormalizedName { get; set; }
+    public required string Description { get; set; }
+
+    public ICollection<Group> Groups { get; set; } = null!;
+}
