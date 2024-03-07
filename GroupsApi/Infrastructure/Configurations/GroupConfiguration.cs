1836f2ed04eb5eba65697b5c405af1431d458908
HTFDT <almetov_tag@mail.ru>
2024-03-07 17:15:10 +0500

    add groups table configuration

---
+++ GroupsApi/Infrastructure/Configurations/GroupConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal sealed class GroupConfiguration : IEntityTypeConfiguration<Group>
+{
+    public void Configure(EntityTypeBuilder<Group> builder)
+    {
+        builder.ToTable("Groups");
+    }
+}
