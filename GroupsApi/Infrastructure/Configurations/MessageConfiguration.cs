4f5b2a846762fb92d0d525ae7bb49dc6d81dfee1
HTFDT <almetov_tag@mail.ru>
2024-03-07 20:25:44 +0500

    add messages table configuration

---
+++ GroupsApi/Infrastructure/Configurations/MessageConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class MessageConfiguration : IEntityTypeConfiguration<Message>
+{
+    public void Configure(EntityTypeBuilder<Message> builder)
+    {
+        builder.ToTable("Messages");
+
+        builder.HasOne(e => e.Group)
+            .WithMany(e => e.Messages)
+            .HasForeignKey(e => e.GroupId);
+
+        builder.HasIndex(e => new { e.GroupId, e.GroupNumber })
+            .IsUnique();
+
+        builder.HasOne(e => e.Membership)
+            .WithMany(e => e.Messages)
+            .HasForeignKey(e => e.MembershipId);
+    }
+}
