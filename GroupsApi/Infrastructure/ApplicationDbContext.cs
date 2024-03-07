daebf902b93fbefe395fe53af369692b4d97dcdd
HTFDT <almetov_tag@mail.ru>
2024-03-07 17:14:59 +0500

    add infrastructure application db context

---
+++ GroupsApi/Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+internal class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Group> Groups { get; set; } = null!;
+    public DbSet<Tag> Tags { get; set; } = null!;
+    public DbSet<GroupRole> GroupRoles { get; set; } = null!;
+    public DbSet<Message> Messages { get; set; } = null!;
+    public DbSet<GroupRight> GroupRights { get; set; } = null!;
+    public DbSet<Membership> Memberships { get; set; } = null!;
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder) => 
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    
+}
