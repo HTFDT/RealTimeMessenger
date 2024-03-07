7c9a46203b63930d89cc7eb8d8ef65c75c346f05
HTFDT <almetov_tag@mail.ru>
2024-03-07 20:25:27 +0500

    add tags table configuration

---
+++ GroupsApi/Infrastructure/Configurations/TagConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class TagConfiguration: IEntityTypeConfiguration<Tag>
+{
+    public void Configure(EntityTypeBuilder<Tag> builder)
+    {
+        builder.ToTable("Tags");
+
+        builder.HasIndex(e => e.NormalizedName)
+            .IsUnique();
+
+        builder.Property(e => e.NormalizedName)
+            .HasComputedColumnSql("UPPER(\"Name\")", true);
+    }
+}
