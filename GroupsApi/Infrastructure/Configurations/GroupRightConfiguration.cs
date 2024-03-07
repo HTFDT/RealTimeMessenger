a9b8813b80521dc20afed6b9f7ca7893afd24341
HTFDT <almetov_tag@mail.ru>
2024-03-07 20:11:52 +0500

    add groupRights table configuration

---
+++ GroupsApi/Infrastructure/Configurations/GroupRightConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class GroupRightConfiguration : IEntityTypeConfiguration<GroupRight>
+{
+    public void Configure(EntityTypeBuilder<GroupRight> builder)
+    {
+        builder.ToTable("GroupRights");
+        
+        builder.Property(e => e.NormalizedName)
+            .HasComputedColumnSql("UPPER(\"Name\")", true);
+
+        builder.HasIndex(e => e.NormalizedName)
+            .IsUnique();
+    }
+}
