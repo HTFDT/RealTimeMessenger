37fc695e6ea1723020f4b53cb5d520009c15cbcd
HTFDT <almetov_tag@mail.ru>
2024-03-07 20:09:47 +0500

    add groupRoles table configuration

---
+++ GroupsApi/Infrastructure/Configurations/GroupRoleConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configurations;
+
+internal class GroupRoleConfiguration : IEntityTypeConfiguration<GroupRole>
+{
+    public void Configure(EntityTypeBuilder<GroupRole> builder)
+    {
+        builder.ToTable("GroupRoles");
+        
+        builder.Property(e => e.NormalizedName)
+            .HasComputedColumnSql("UPPER(\"Name\")", true);
+
+        builder.HasIndex(e => e.IsDefaultOwnerRole)
+            .HasFilter("\"IsDefaultOwnerRole\" = true")
+            .IsUnique();
+        
+        builder.HasIndex(e => e.IsDefaultMemberRole)
+            .HasFilter("\"IsDefaultMemberRole\" = true")
+            .IsUnique();
+        
+        builder.HasIndex(e => e.IsDefaultKickedRole)
+            .HasFilter("\"IsDefaultKickedRole\" = true")
+            .IsUnique();
+
+        builder.HasIndex(e => e.IsDefaultLeftRole)
+            .HasFilter("\"IsDefaultLeftRole\" = true")
+            .IsUnique();
+
+        builder.HasIndex(e => new { e.GroupId, e.NormalizedName })
+            .IsUnique();
+    }
+}
