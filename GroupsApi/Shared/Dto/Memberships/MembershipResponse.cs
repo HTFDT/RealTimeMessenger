1f508330f1e02576002706f2bd7d623965e54e77
HTFDT <almetov_tag@mail.ru>
2024-03-08 18:51:38 +0500

    add memberships dtos

---
+++ GroupsApi/Shared/Dto/Memberships/MembershipResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Shared.Dto.Memberships;
+
+public record MembershipResponse(Guid Id,
+    Guid UserId,
+    Guid GroupId,
+    Guid? GroupRoleId, 
+    DateTime DateJoined,
+    bool IsKicked,
+    DateTime? DateKicked);
