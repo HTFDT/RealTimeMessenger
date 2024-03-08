f4eff4e62eaf2822499c69fc3e0d120521118302
HTFDT <almetov_tag@mail.ru>
2024-03-08 18:01:12 +0500

    add memberships repository implementation

---
+++ GroupsApi/Infrastructure/Repositories/MembershipsRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class MembershipsRepository(ApplicationDbContext context) : BaseRepository<Membership>(context), IMembershipsRepository
+{
+    public Task<Membership?> GetByUserIdAndGroupId(Guid userId, Guid groupId)
+    {
+        return Entities.SingleOrDefaultAsync(e => e.UserId == userId && e.GroupId == groupId);
+    }
+
+    public Task<Membership?> GetByMemberIdAndGroupId(Guid groupId, Guid memberId)
+    {
+        return Entities.SingleOrDefaultAsync(e => e.Id == memberId && e.GroupId == groupId);
+    }
+
+    public Task<List<Guid>> GetUsersInGroup(Guid groupId, bool includeKicked)
+    {
+        return Entities.Where(e => e.GroupId == groupId && (includeKicked || !e.IsKicked))
+            .Select(e => e.UserId)
+            .ToListAsync();
+    }
+}
