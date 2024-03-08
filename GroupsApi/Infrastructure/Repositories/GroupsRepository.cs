7e869f13c22ed0e9b165ab1a736aa8aadb86f512
HTFDT <almetov_tag@mail.ru>
2024-03-08 15:00:15 +0500

    add groups repository implementation

---
+++ GroupsApi/Infrastructure/Repositories/GroupsRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Linq.Expressions;
+using Domain.Entities;
+using Domain.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+internal class GroupsRepository(ApplicationDbContext context) : BaseRepository<Group>(context), IGroupsRepository
+{
+   
+}
