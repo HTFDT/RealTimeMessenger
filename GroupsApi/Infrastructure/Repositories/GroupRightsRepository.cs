7d10e3c32bed20fd6dd3b4e69537cc6be7a3a15b
HTFDT <almetov_tag@mail.ru>
2024-03-08 18:21:10 +0500

    add groupRights repository implementation

---
+++ GroupsApi/Infrastructure/Repositories/GroupRightsRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class GroupRightsRepository(ApplicationDbContext context) 
+    : BaseRepository<GroupRight>(context), IGroupRightsRepository
+{
+    public Task<GroupRight?> GetByGroupRightNameAsync(string name)
+    {
+        return Entities.SingleOrDefaultAsync(gr => gr.NormalizedName == name.ToUpper());
+    }
+}
