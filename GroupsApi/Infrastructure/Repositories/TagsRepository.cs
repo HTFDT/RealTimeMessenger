7678439de21b3745c0af45053afac51033c761b9
HTFDT <almetov_tag@mail.ru>
2024-03-08 16:55:05 +0500

    add tags repository implementation

---
+++ GroupsApi/Infrastructure/Repositories/TagsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+internal class TagsRepository(ApplicationDbContext context) : BaseRepository<Tag>(context), ITagsRepository
+{
+    public Task<Tag?> GetByTagNameAsync(string tagName)
+    {
+        return Entities.SingleOrDefaultAsync(e => e.NormalizedName == tagName.ToUpper());
+    }
+}
