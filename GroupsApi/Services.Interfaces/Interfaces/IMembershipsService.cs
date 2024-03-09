88f604b6f0d8150ee70861716c71abcf20601adf
HTFDT <almetov_tag@mail.ru>
2024-03-09 15:11:03 +0500

    add memberships service interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IMembershipsService.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Dto.Groups;
+using Shared.Dto.Memberships;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface IMembershipsService
+{
+    Task<MembershipResponse> JoinGroup(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> AddMemberToPublicGroup(Guid requesterId, Guid groupId, AddMemberRequest request);
+    Task<MembershipResponse> AddMemberToPrivateGroup(Guid requesterId, Guid groupId, AddMemberRequest request);
+    Task<MembershipResponse> KickMember(Guid requesterId, Guid groupId, Guid kickedId);
+    Task<IEnumerable<MembershipResponse>> GetAllMembers(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> GetMember(Guid requesterId, Guid groupId, Guid memberId);
+    Task<MembershipResponse> LeaveGroup(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> AssignRoleToMember(Guid requesterId, Guid groupId, Guid memberId, AssignRoleToMemberRequest request);
+}
