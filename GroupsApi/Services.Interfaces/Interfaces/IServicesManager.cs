2aa05cfedba0b4d536c0e43ac1bde0f86997859d
HTFDT <almetov_tag@mail.ru>
2024-03-09 20:07:48 +0500

    add service manager interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IServicesManager.cs
@@ -0,0 +1,11 @@
+﻿namespace Services.Interfaces.Interfaces;
+
+public interface IServicesManager
+{
+    public IGroupsService Groups { get; }
+    public IMessagesServiceWrapper Messages { get; }
+    public IMembershipsServiceWrapper Memberships { get; }
+    public IGroupRolesService GroupRoles { get; }
+    public IGroupRightsService GroupRights { get; }
+    public ITagsService Tags { get; }
+}
