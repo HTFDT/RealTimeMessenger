faeddbae0e5a7017ba5b84d93cc767ac53d33a81
HTFDT <almetov_tag@mail.ru>
2024-03-09 13:55:46 +0500

    add group service interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IGroupsService.cs
@@ -0,0 +1,16 @@
+﻿using Shared.Dto.Groups;
+using Shared.Dto.Memberships;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface IGroupsService
+{
+    Task<IEnumerable<GroupResponse>> GetAllGroups();
+    Task<GroupResponse> GetGroup(Guid groupId);
+    Task<GroupResponse> CreateGroup(Guid requesterId, CreateGroupRequest request);
+    Task<GroupResponse> UpdateGroup(Guid requestedId, Guid groupId, UpdateGroupRequest request);
+    Task DeleteGroup(Guid requesterId, Guid groupId);
+    Task AddTag(Guid requesterId, Guid groupId, AddTagRequest request);
+    Task RemoveTag(Guid requesterId, Guid groupId, Guid tagId);
+    Task<GroupResponse> SetDefaultMemberRole(Guid requesterId, Guid groupId, SetDefaultMemberRoleRequest request);
+}
