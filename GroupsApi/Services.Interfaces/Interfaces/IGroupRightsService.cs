df5fe0b6e4393ea80ffa1b74c6c8a431e3be5dc3
HTFDT <almetov_tag@mail.ru>
2024-03-09 16:20:17 +0500

    add groupRights service interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IGroupRightsService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Dto.GroupRights;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface IGroupRightsService
+{
+    Task<GroupRightResponse> GetGroupRight(Guid groupRightId);
+    Task<IEnumerable<GroupRightResponse>> GetAllGroupRights();
+    Task<GroupRightResponse> UpdateGroupRight(Guid groupRightId, UpdateGroupRightRequest request);
+    Task<GroupRightResponse> CreateGroupRight(Guid groupRightId, CreateGroupRightRequest request);
+    Task DeleteGroupRight(Guid groupRightId);
+}
