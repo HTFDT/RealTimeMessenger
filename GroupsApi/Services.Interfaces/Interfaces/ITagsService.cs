a8ddf8d1423d2e9143167d264ad84ebf7216d42c
HTFDT <almetov_tag@mail.ru>
2024-03-09 14:00:28 +0500

    add tag service interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/ITagsService.cs
@@ -0,0 +1,12 @@
+﻿using Shared.Dto.Tags;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface ITagsService
+{
+    Task<IEnumerable<TagResponse>> GetAllTags();
+    Task<TagResponse> GetTag(Guid tagId);
+    Task<TagResponse> CreateTag(CreateTagRequest request);
+    Task<TagResponse> UpdateTag(Guid tagId, UpdateTagRequest request);
+    Task DeleteTag(Guid tagId);
+}
