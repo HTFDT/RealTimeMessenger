d2e0af1293319546d95d7cbdb550e039c41535ba
HTFDT <almetov_tag@mail.ru>
2024-03-10 20:53:07 +0500

    add message service wrapper interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IMessagesServiceWrapper.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Dto.Messages;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface IMessagesServiceWrapper
+{
+    Task<IEnumerable<MessageResponse>> GetMessagesInGroup(Guid requesterId, Guid groupId, GetMessagesRequest request);
+    Task<MessageResponse> GetMessageInGroup(Guid requesterId, Guid groupId, long groupNumber);
+    Task<MessageResponse> GetMessageInGroup(Guid requesterId, Guid groupId, Guid messageId);
+    Task<MessageResponse> CreateMessage(Guid requesterId, Guid groupId, CreateMessageRequest request);
+    Task<MessageResponse> EditMessage(Guid requesterId, Guid groupId, long groupNumber, EditMessageRequest request);
+    Task<MessageResponse> EditMessage(Guid requesterId, Guid groupId, Guid messageId, EditMessageRequest request);
+    Task DeleteMessage(Guid requesterId, Guid groupId, long groupNumber);
+    Task DeleteMessage(Guid requesterId, Guid groupId, Guid messageId);
+    Task<MessageResponse> PinMessage(Guid requesterId, Guid groupId, Guid messageId);
+    Task<MessageResponse> PinMessage(Guid requesterId, Guid groupId, long groupNumber);
+    Task<MessageResponse> UnpinMessage(Guid requesterId, Guid groupId, Guid messageId);
+    Task<MessageResponse> UnpinMessage(Guid requesterId, Guid groupId, long groupNumber);
+}
