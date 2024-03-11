3f1e657a296a9d8b80ecc587066480c56ba5a511
HTFDT <almetov_tag@mail.ru>
2024-03-11 19:38:11 +0500

    add memberships service wrapper interface

---
+++ GroupsApi/Services.Interfaces/Interfaces/IMembershipsServiceWrapper.cs
@@ -0,0 +1,15 @@
+﻿using Shared.Dto.Groups;
+using Shared.Dto.Memberships;
+
+namespace Services.Interfaces.Interfaces;
+
+public interface IMembershipsServiceWrapper
+{
+    Task<MembershipResponse> JoinGroup(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> AddMember(Guid requesterId, Guid groupId, AddMemberRequest request);
+    Task<MembershipResponse> KickMember(Guid requesterId, Guid groupId, Guid kickedId);
+    Task<IEnumerable<MembershipResponse>> GetAllMembers(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> GetMember(Guid requesterId, Guid groupId, Guid memberId);
+    Task<MembershipResponse> LeaveGroup(Guid requesterId, Guid groupId);
+    Task<MembershipResponse> AssignRoleToMember(Guid requesterId, Guid groupId, Guid memberId, AssignRoleToMemberRequest request);
+}
