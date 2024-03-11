e357f630c63abaf1e3f654fe6b7717d0736a32c0
HTFDT <almetov_tag@mail.ru>
2024-03-11 21:40:30 +0500

    add ensure requester rights custom attribute

---
+++ GroupsApi/Services/Attributes/EnsureRequesterRightsAttribute.cs
@@ -0,0 +1,6 @@
+﻿namespace Services.Attributes;
+
+internal class EnsureRequesterRightsAttribute(params string[] rightNames) : Attribute
+{
+    public string[] RightsNames { get; } = rightNames;
+}
