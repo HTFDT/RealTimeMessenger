2fe939e47a20fb49d6e0d4a5532531a262f2c4d4
HTFDT <almetov_tag@mail.ru>
2024-03-12 23:24:30 +0500

    add groups api service layer service collection extensions

---
+++ GroupsApi/Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Services.Implementations;
+using Services.Interfaces.Interfaces;
+
+namespace Services.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddScoped<IServicesManager, ServicesManager>();
+        return services;
+    }
+}
