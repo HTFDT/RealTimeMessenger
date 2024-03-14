54282d7599c60c09765065a6fe10664654d9f9a7
HTFDT <almetov_tag@mail.ru>
2024-03-14 20:43:39 +0500

    add groups api base controller

---
+++ GroupsApi/Presentation/Controllers/BaseGroupsApiController.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+public abstract class BaseGroupsApiController : Controller
+{
+    protected const string UnauthorizedMessage = "Can't get requester id from token";
+    
+    protected bool TryGetUserIdFromClaims(out Guid userId)
+    {
+        var claim = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier);
+        if (claim is not null && Guid.TryParse(claim.Value, out userId)) 
+            return true;
+        userId = Guid.Empty;
+        return false;
+    }
+}
