bd16c9e45278e8d48ee92f18def7f2dfd15b1bc5
HTFDT <almetov_tag@mail.ru>
2024-03-16 20:39:37 +0500

    add core global exception handling middleware

---
+++ Libs/Core/Base/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using Core.Base.Exceptions;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Base.Middleware;
+
+public sealed class ExceptionHandlingMiddleware : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        try
+        {
+            await next(context);
+        }
+        catch (Exception e)
+        {
+            await HandleExceptionAsync(context, e);
+        }
+    }
+
+    private static async Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.StatusCode = exception switch
+        {
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            ConflictException => StatusCodes.Status409Conflict,
+            InternalServerErrorException => StatusCodes.Status500InternalServerError,
+            ForbiddenException => StatusCodes.Status403Forbidden,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        var response = new { error = exception.Message };
+
+        await context.Response.WriteAsJsonAsync(response);
+    }
+}
