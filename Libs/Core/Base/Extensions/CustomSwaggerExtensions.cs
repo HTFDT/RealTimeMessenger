27d0d1e53c9340ea1d881205fc83c7976e4a88ea
HTFDT <almetov_tag@mail.ru>
2024-03-16 20:05:58 +0500

    add core authentication and authorization logic

---
+++ Libs/Core/Base/Extensions/CustomSwaggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace Core.Base.Extensions;
+
+public static class CustomSwaggerExtensions
+{
+    public static IServiceCollection AddCustomSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options=>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter 'Bearer [jwt]'",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey
+            });
+            var scheme = new OpenApiSecurityScheme 
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            };
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement { { scheme, Array.Empty<string>() } });
+        });
+        return services;
+    }
+}
