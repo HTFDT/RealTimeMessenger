338675b3c2b5315ed1b93bb603f8fe65bbe74b63
HTFDT <almetov_tag@mail.ru>
2024-03-30 19:30:04 +0500

    add jwt token accessor logic

---
+++ Libs/Core/Jwt/JwtTokenAccessor.cs
@@ -0,0 +1,20 @@
+﻿using Core.Jwt.Interfaces;
+
+namespace Core.Jwt;
+
+internal class JwtTokenAccessor : IJwtTokenAccessor
+{
+    public string Name => "Authorization";
+
+    private string _value = null!;
+    
+    public string ReadValue()
+    {
+        return _value;
+    }
+
+    public void WriteValue(string value)
+    {
+        _value = value;
+    }
+}
