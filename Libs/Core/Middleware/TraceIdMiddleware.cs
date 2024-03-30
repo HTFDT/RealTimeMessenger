4588c275a7e4720420c6a1bcd56c6d5e1c759661
HTFDT <almetov_tag@mail.ru>
2024-03-30 20:56:43 +0500

    add trace id middleware

---
+++ Libs/Core/Middleware/TraceIdMiddleware.cs
@@ -0,0 +1,19 @@
+﻿using Core.TraceIdLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Middleware;
+
+/// <summary>
+/// Миддлварь для того, чтобы добавлять значение TraceId, если его еще нет.
+/// Регистрирутеся в каждом сервисе строго после TraceWriterMiddleware
+/// </summary>
+/// <param name="traceIdAccessor"></param>
+public class TraceIdMiddleware(ITraceIdAccessor traceIdAccessor) : IMiddleware
+{
+    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        if (traceIdAccessor.ReadValue() is null)
+            traceIdAccessor.WriteValue(Guid.NewGuid().ToString());
+        return next(context);
+    }
+}
