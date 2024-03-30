3511b1f279f9c17defc060f9d527731775e41982
HTFDT <almetov_tag@mail.ru>
2024-03-30 18:34:20 +0500

    add identity api connection service

---
+++ GroupsApi/IdentityConnectionLib/Exceptions/UserInfoNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿using Core.Base.Exceptions;
+
+namespace IdentityConnectionLib.Exceptions;
+
+public class UserInfoNotFoundException : NotFoundException
+{
+    public UserInfoNotFoundException(Guid id) : 
+        base($"the info for the user with the identifier '{id}' was not found")
+    {
+    }
+}
