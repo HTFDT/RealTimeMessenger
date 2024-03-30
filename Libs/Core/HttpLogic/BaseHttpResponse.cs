d1819b3b5dd635d1d0db74dcc2082b12d945a701
HTFDT <almetov_tag@mail.ru>
2024-03-30 18:33:02 +0500

    add core http logic

---
+++ Libs/Core/HttpLogic/BaseHttpResponse.cs
@@ -0,0 +1,35 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace Core.HttpLogic;
+
+public record BaseHttpResponse
+{
+    /// <summary>
+    /// Статус ответа
+    /// </summary>
+    public required HttpStatusCode StatusCode { get; set; }
+
+    /// <summary>
+    /// Заголовки, передаваемые в ответе
+    /// </summary>
+    public required HttpResponseHeaders Headers { get; set; }
+
+    /// <summary>
+    /// Заголовки контента
+    /// </summary>
+    public required HttpContentHeaders ContentHeaders { get; init; }
+
+    /// <summary>
+    /// Является ли статус код успешным
+    /// </summary>
+    public bool IsSuccessStatusCode
+    {
+        get
+        {
+            var statusCode = (int)StatusCode;
+
+            return statusCode >= 200 && statusCode <= 299;
+        }
+    }
+}
---
+++ Libs/Core/HttpLogic/HttpRequestService.cs
@@ -0,0 +1,104 @@
+﻿using System.Net.Http.Json;
+using System.Net.Mime;
+using System.Text;
+using Core.HttpLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ContentType = Core.HttpLogic.Enums.ContentType;
+
+namespace Core.HttpLogic;
+
+/// <inheritdoc />
+internal class HttpRequestService(IHttpConnectionService httpConnectionService) : IHttpRequestService
+{
+    /// <inheritdoc />
+    public async Task<HttpResponse<TResponse>> SendRequestAsync<TResponse>(HttpRequestData requestData, 
+        HttpConnectionData connectionData)
+    {
+        var client = httpConnectionService.CreateHttpClient(connectionData);
+        HttpContent? content = null;
+        if (requestData.Body is not null) 
+            content = PrepareContent(requestData.Body, requestData.ContentType);
+        var uri = new Uri(QueryHelpers.AddQueryString(requestData.Path, requestData.QueryParameterList));
+        var httpRequestMessage = new HttpRequestMessage(requestData.Method, uri);
+        httpRequestMessage.Content = content;
+        foreach (var header in requestData.HeaderDictionary)
+            httpRequestMessage.Headers.Add(header.Key, header.Value);
+
+        var res = await httpConnectionService.SendRequestAsync(httpRequestMessage, client, default);
+
+        var response = new HttpResponse<TResponse>
+        {
+            Body = await res.Content.ReadFromJsonAsync<TResponse>(),
+            StatusCode = res.StatusCode,
+            Headers = res.Headers,
+            ContentHeaders = res.Content.Headers
+        };
+        return response;
+    }
+
+    private static HttpContent PrepareContent(object body, ContentType contentType)
+    {
+        switch (contentType)
+        {
+            case ContentType.ApplicationJson:
+            {
+                if (body is string stringBody)
+                {
+                    body = JToken.Parse(stringBody);
+                }
+
+                var serializeSettings = new JsonSerializerSettings
+                {
+                    ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                    NullValueHandling = NullValueHandling.Ignore
+                };
+                var serializedBody = JsonConvert.SerializeObject(body, serializeSettings);
+                var content = new StringContent(serializedBody, Encoding.UTF8, MediaTypeNames.Application.Json);
+                return content;
+            }
+
+            case ContentType.XWwwFormUrlEncoded:
+            {
+                if (body is not IEnumerable<KeyValuePair<string, string>> list)
+                {
+                    throw new Exception(
+                        $"Body for content type {contentType} must be {typeof(IEnumerable<KeyValuePair<string, string>>).Name}");
+                }
+
+                return new FormUrlEncodedContent(list);
+            }
+            case ContentType.ApplicationXml:
+            {
+                if (body is not string s)
+                {
+                    throw new Exception($"Body for content type {contentType} must be XML string");
+                }
+
+                return new StringContent(s, Encoding.UTF8, MediaTypeNames.Application.Xml);
+            }
+            case ContentType.Binary:
+            {
+                if (body.GetType() != typeof(byte[]))
+                {
+                    throw new Exception($"Body for content type {contentType} must be {typeof(byte[]).Name}");
+                }
+
+                return new ByteArrayContent((byte[])body);
+            }
+            case ContentType.TextXml:
+            {
+                if (body is not string s)
+                {
+                    throw new Exception($"Body for content type {contentType} must be XML string");
+                }
+
+                return new StringContent(s, Encoding.UTF8, MediaTypeNames.Text.Xml);
+            }
+            default:
+                throw new ArgumentOutOfRangeException(nameof(contentType), contentType, null);
+        }
+    }
+}
