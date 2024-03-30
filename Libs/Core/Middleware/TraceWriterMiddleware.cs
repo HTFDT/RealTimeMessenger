ae4bb648f19515acbdfd1236ef3e4fcb2cc0a42e
HTFDT <almetov_tag@mail.ru>
2024-03-30 20:00:28 +0500

    add trace writer middleware

---
+++ Libs/Core/Middleware/TraceWriterMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using Core.TraceLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Core.Middleware;
+
+public sealed class TraceWriterMiddleware(IEnumerable<ITraceWriter> traceWriters) : IMiddleware
+{
+    public Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+        foreach (var writer in traceWriters)
+            if (context.Request.Headers.TryGetValue(writer.Name, out var values))
+                writer.WriteValue(string.Join(";", values.ToArray()));
+            
+        return next(context);
+    }
+}
