3d1b0a6c4de856300251cc073ec4ba945b110751
HTFDT <almetov_tag@mail.ru>
2024-04-10 20:40:03 +0500

    add consumer interface

---
+++ Libs/Core/RabbitLogic/Interfaces/IConsumer.cs
@@ -0,0 +1,8 @@
+﻿using RabbitMQ.Client;
+
+namespace Core.RabbitLogic.Interfaces;
+
+public interface IConsumer
+{
+    public void Consume(IConnection connection, string exchange, string routingKey, string exchangeType);
+}
