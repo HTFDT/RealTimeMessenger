a9497ce3fbff781abfe792f5463c0cd5249b84c2
HTFDT <almetov_tag@mail.ru>
2024-04-10 20:53:59 +0500

    change identity connection service collection extensions accordingly

--- Libs/IdentityConnectionLib/ConnectionServices/Extensions/ServiceCollectionExtensions.cs
+++ Libs/IdentityConnectionLib/ConnectionServices/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,26 @@
 ﻿using IdentityConnectionLib.ConnectionServices.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace IdentityConnectionLib.ConnectionServices.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddIdentityConnectionService(this IServiceCollection services)
+    public static IServiceCollection AddIdentityConnectionService(this IServiceCollection services, IConfiguration config)
     {
-        services.AddScoped<IIdentityConnectionService, IdentityConnectionService>();
+        switch (config["ServiceInteraction:Type"])
+        {
+            case "http":
+                services.AddScoped<IIdentityConnectionService, HttpIdentityConnectionService>();
+                break;
+            case "broker":
+                services.AddSingleton<IIdentityConnectionService, RabbitMqIdentityConnectionService>();
+                break;
+            default:
+                services.AddScoped<IIdentityConnectionService, HttpIdentityConnectionService>();
+                break;
+        }
+            
         return services;
     }
 }
