050d0eb4407e00ae973181d29cd45ffd69d0496c
HTFDT <almetov_tag@mail.ru>
2024-04-10 20:40:23 +0500

    add producer interface

---
+++ Libs/Core/RabbitLogic/Interfaces/IProducer.cs
@@ -0,0 +1,8 @@
+﻿using RabbitMQ.Client;
+
+namespace Core.RabbitLogic.Interfaces;
+
+public interface IProducer
+{
+    public Task<string> ProduceAsync(IConnection connection, string body, string exchange, string routingKey, string exchangeType, string replyTo, CancellationToken token = default);
+}
