35dc01e992ae6d9ee253d7bbf4859df3045264c6
HTFDT <almetov_tag@mail.ru>
2024-04-10 20:44:22 +0500

    add client side service collection extensions

---
+++ Libs/Core/RabbitLogic/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Core.RabbitLogic.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Core.RabbitLogic.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRabbitClientServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IRabbitMqClientService, RabbitMqClientService>();
+        return services;
+    }
+}
