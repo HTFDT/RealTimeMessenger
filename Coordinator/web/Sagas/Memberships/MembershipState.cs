db562eb76f500b7603ee38845d14947eda5346b5
HTFDT <almetov_tag@mail.ru>
2024-05-09 16:09:01 +0500

    add membership request saga state machine

---
+++ Coordinator/web/Sagas/Memberships/MembershipState.cs
@@ -0,0 +1,15 @@
+﻿using Core.MassTransit.Messages;
+using MassTransit;
+
+namespace web.Sagas.Memberships;
+
+public class MembershipState : SagaStateMachineInstance
+{
+    public Guid CorrelationId { get; set; }
+    public State CurrentState { get; set; } = null!;
+    public Guid? RequestId { get; set; }
+
+    public MembershipResponse MembershipResponse { get; set; } = null!;
+    public Uri? ResponseAddress { get; set; }
+    public Guid? ProcessingId { get; set; }
+}
