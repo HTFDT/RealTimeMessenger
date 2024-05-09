31b98cd5431c0b91f685223a50fc9558a7c0104b
HTFDT <almetov_tag@mail.ru>
2024-05-09 16:20:37 +0500

    add UserInfoConsumer

---
+++ IdentityApi/Api/Consumers/MassTransitUserInfoConsumer.cs
@@ -0,0 +1,16 @@
+﻿using Core.MassTransit.Messages;
+using Logic;
+using MassTransit;
+
+namespace Api.Consumers;
+
+public class MassTransitUserInfoConsumer(UserManager manager) : IConsumer<UserInfoRequest>
+{
+    public async Task Consume(ConsumeContext<UserInfoRequest> context)
+    {
+        var user = await manager.GetById(context.Message.Id);
+        if (user is null)
+            throw new InvalidOperationException("User not found");
+        await context.RespondAsync(new UserInfoResponse { Id = user.Id, Username = user.Username });
+    }
+}
